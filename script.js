8cac1735dfa9f55e67d25e992ad3e2f70af4bc3a
Sr e Sra Queiroz <welbertq32@gmail.com>
2024-10-02 13:43:05 -0300

    Html finalizado

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let logo = document.getElementById("logo")
 
 let postAutores = document.getElementsByClassName("post-autor")
+
+let post2 = document.getElementById("post02")
+
+let formulario = document.getElementById("formulário")
+
+let ambosPosts = document.getElementsByClassName("post")
+
+let dataPosts = document.getElementsByClassName("post-data")
+
+let textoPosts = document.getElementsByClassName("post-texto")
+
+let redesSociais = document.getElementsByClassName("lista_redes")
